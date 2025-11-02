1e1794b9274e4d2fc0ba27ec4804ae9a47bb80a7
Vladimir Baganov <baganov.v@gmail.com>
2025-11-02 22:51:30 +0300

    первый коммит

---
+++ src/test/java/com/baganov/klassifikator/KlassifikatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baganov.klassifikator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlassifikatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
